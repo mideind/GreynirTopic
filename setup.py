05e96cbb6530a8b6e2cbb95c952ec3a86dc672c1
Vilhjalmur Thorsteinsson <vt@extrada.com>
2020-06-12 18:43:22 +0000

    Added setup.py and py.typed

---
+++ setup.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+    Greynir: Natural language processing for Icelandic
+
+    Setup.py
+
+    Copyright (C) 2020 Miðeind ehf.
+    Original author: Vilhjálmur Þorsteinsson
+
+    This software is licensed under the MIT License:
+
+        Permission is hereby granted, free of charge, to any person
+        obtaining a copy of this software and associated documentation
+        files (the "Software"), to deal in the Software without restriction,
+        including without limitation the rights to use, copy, modify, merge,
+        publish, distribute, sublicense, and/or sell copies of the Software,
+        and to permit persons to whom the Software is furnished to do so,
+        subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be
+        included in all copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+        CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+        TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+        SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+    This module sets up the GreynirTopic package.
+
+    This package requires Python >= 3.5, and supports PyPy >= 3.5.
+
+"""
+
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import io
+import re
+import sys
+
+from glob import glob
+from os.path import basename, dirname, join, splitext
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+if sys.version_info < (3, 5):
+    print("GreynirTopic requires Python >= 3.5")
+    sys.exit(1)
+
+
+def read(*names, **kwargs):
+    try:
+        return io.open(
+            join(dirname(__file__), *names),
+            encoding=kwargs.get("encoding", "utf8")
+        ).read()
+    except (IOError, OSError):
+        return ""
+
+
+setup(
+    name="greynir-topic",
+    # Remember to update version in src/greynir_topic/__init__.py as well
+    version="0.0.1",
+    license="MIT",
+    description="Text similarity and topic indexing for Icelandic",
+    long_description="{0}\n{1}".format(
+        re
+            .compile("^.. start-badges.*^.. end-badges", re.M | re.S)
+            .sub("", read("README.rst")),
+        re
+            .sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
+    ),
+    author="Miðeind ehf",
+    author_email="mideind@mideind.is",
+    url="https://github.com/mideind/GreynirTopic",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
+    package_data={"greynir_topic": ["py.typed"]},
+    include_package_data=True,
+    zip_safe=True,
+    classifiers=[
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Natural Language :: Icelandic",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    keywords=["topic", "similarity", "tf-idf", "lsi", "icelandic"],
+    setup_requires=[],
+    install_requires=[],
+    entry_points={},
+)
