696dec9a2f262c94583d33fe67efd4ffc541c56c
JohannSJohannsson <jsj13@hi.is>
2020-08-07 10:34:37 +0000

    corrected names that didn't fall under Pythons coding conventions.

--- src/greynir_topic/text_to_lemmas.py
+++ src/greynir_topic/text_to_lemmas.py
@@ -8,9 +8,9 @@ class GenTokens():
         self.text = text
     
     def get_tokens(self):
-        tokenList = list(tokenizer.tokenize(self.text))
-        tokenText = tokenizer.text_from_tokens(tokenList)
-        return tokenText
+        token_list = list(tokenizer.tokenize(self.text))
+        token_text = tokenizer.text_from_tokens(token_list)
+        return token_text
 
 class GenTaggedText():
     """
@@ -20,16 +20,16 @@ class GenTaggedText():
         returns a subprocess.Popen object which contains the tagged text.
         This is temporary and will be replaced.
     """
-    def __init__(self, tokenized_Text):
-        self.tokenized_Text = tokenized_Text
+    def __init__(self, tokenized_text):
+        self.tokenized_text = tokenized_text
     
-    def iceTagger_tag(self):
+    def icetagger_tag(self):
         """ IceTagger is a temporary tagger. To be replaced by a more suitible tagger """
         os.chdir("IceNLP")
         os.chdir("bat")
         os.chdir("icetagger")
         with open("tokenized.txt", 'w') as f:
-            f.write(self.tokenized_Text)
+            f.write(self.tokenized_text)
         pipe = subprocess.Popen("cat 'tokenized.txt' | ./icetagger.sh -of 1", stdout=subprocess.PIPE,stderr=None, shell=True)
         for _ in range(3):
             os.chdir("..")
@@ -40,8 +40,8 @@ class GenLemmas():
         This class uses Nefnir to lemmatize tagged text. It recieves tagged text and
         creates both a tagged text file and a lemmatized text file.
     """
-    def __init__(self, tagged_Text):
-        self.tagged_text = tagged_Text
+    def __init__(self, tagged_text):
+        self.tagged_text = tagged_text
     
     def nefnir_lemmatize(self):
         os.chdir("nefnir-master")
@@ -59,34 +59,34 @@ class ConvertText():
     """
     def __init__(self, filename):
         self.filename = filename
-        self.tokText = GenTokens(open(self.filename)).get_tokens()
-        self.taggedText = GenTaggedText(self.tokText)
+        self.tok_text = GenTokens(open(self.filename)).get_tokens()
+        self.tagged_text = GenTaggedText(self.tok_text)
     
     def lemmatize_text(self):
-        t = self.taggedText.iceTagger_tag()
+        t = self.tagged_text.icetagger_tag()
         fully_tagged_text = t.communicate()[0].decode('utf-8').replace(' <UNKNOWN>','')
         GenLemmas(fully_tagged_text).nefnir_lemmatize()
         with open('lemmatized.txt', 'r') as f:
             reader = csv.reader(f)
-            allRows = [row for row in reader]
-        allLemmas = list()
-        for idx, row in enumerate(allRows):
-            if allRows[idx]:
-                if len(allRows[idx][0].split()) == 3:
-                    allLemmas.append(allRows[idx][0].split()[2])
-        print(allLemmas)
-        print(len(allLemmas))
+            all_rows = [row for row in reader]
+        all_lemmas = list()
+        for idx, row in enumerate(all_rows):
+            if all_rows[idx]:
+                if len(all_rows[idx][0].split()) == 3:
+                    all_lemmas.append(all_rows[idx][0].split()[2])
+        print(all_lemmas)
+        print(len(all_lemmas))
 
     def create_as_json(self):
         """ Creates a json file containing tokens, lemmas and tags from arnastofnun.is """
         os.system("curl -X POST -F \"text=`cat "+self.filename+"`\" -F \"lemma=on\" -F \"expand_tag=on\" malvinnsla.arnastofnun.is > text_arnastofnun.json")
         with open('text_arnastofnun.json') as f:
             data = json.load(f)
-        allLemmas = list()
+        all_lemmas = list()
         for paragraph in data['paragraphs']:
             for sentence in paragraph['sentences']:
                 for lemmas in sentence:
-                    allLemmas.append(lemmas['lemma'])
-        print(allLemmas)
-        print(len(allLemmas))
+                    all_lemmas.append(lemmas['lemma'])
+        print(all_lemmas)
+        print(len(all_lemmas))
 
